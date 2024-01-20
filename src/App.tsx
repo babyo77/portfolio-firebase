d769adadbe54c8522adc190c1dd9a064ca5e854a
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-20 20:26:07 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,9 @@ function App() {
         {Details ? (
           Details && (
             <>
-              <main className="container py-8  ">
+           <div className="absolute inset-0 -z-10 h-full w-full  bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
+           
+              <main className="container py-8">
                 <Header
                   name={Details?.name}
                   username={Details?.username}
