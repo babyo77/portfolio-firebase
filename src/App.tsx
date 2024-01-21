2a3887e80ee6cc1a3ce9bde6a9f92709877c5f53
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-21 12:40:03 +0530

    hehehe

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         {Details ? (
           Details && (
             <>
-        <div className="absolute top-0 z-[-2] h-screen w-screen  bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+        <div className="absolute top-0 z-[-2] h-screen w-full  bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
            
               <main className="container fade-in py-8">
                 <Header
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,41 +1,55 @@
 import { ProjectProps } from "@/interface";
-import { Loader, Projects} from ".";
+import { Loader, Projects } from ".";
 import { Badge } from "./ui/badge";
 
-interface ProjectProp{
-    project:ProjectProps[],
-    techStack:string[]
+interface ProjectProp {
+  project: ProjectProps[];
+  techStack: string[];
 }
 
-const Container:React.FC<ProjectProp> = ({project,techStack}) => {
+const Container: React.FC<ProjectProp> = ({ project, techStack }) => {
   return (
     <div className=" container flex flex-col  space-y-2 ">
       <article>
-      <div className="pt-4">
-          <h2 className="font-mono text-lg tracking-tighter font-semibold">Tech-Stack</h2>
+        <div className="pt-4">
+          <h2 className="font-mono text-lg tracking-tighter font-semibold">
+            Tech-Stack
+          </h2>
         </div>
         <div className="mt-4 mb-1 flex items-center gap-1 flex-wrap ">
-         {techStack.map((tech,id)=>(
-            <Badge key={id} variant={"outline"} className="max-md:text-xs text-sm cursor-pointer hover:bg-zinc-100/5 hover:border-blue-300  hover:font-semibold transition-all duration-300 font-normal rounded-none shadow-none">{tech}</Badge>
-         ))}
+          {techStack.map((tech, id) => (
+            <Badge
+              key={id}
+              variant={"outline"}
+              className="max-md:text-xs text-sm cursor-pointer hover:bg-zinc-100/5 hover:border-blue-300  transition-all duration-300 font-normal rounded-none shadow-none"
+            >
+              {tech}
+            </Badge>
+          ))}
         </div>
         <div className="pt-4">
-          <h2 className="font-mono text-lg tracking-tighter font-semibold">Projects</h2>
+          <h2 className="font-mono text-lg tracking-tighter font-semibold">
+            Projects
+          </h2>
         </div>
-      
-{!project? ( <div className="space-y-3 py-3 min-h-96 flex flex-col justify-center items-center">
-        <Loader/> 
-        </div>) : (
-  <ul className="space-y-3 py-3  flex flex-col ">
-  {project.map((data,id)=>(
-     <Projects key={id} title={data.title} tech={data.tech} link={data.link} desc={data.desc}  />
-  ))}
- </ul>
+
+        {!project ? (
+          <div className="space-y-3 py-3 min-h-96 flex flex-col justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <ul className="space-y-3 py-3  flex flex-col ">
+            {project.map((data, id) => (
+              <Projects
+                key={id}
+                title={data.title}
+                tech={data.tech}
+                link={data.link}
+                desc={data.desc}
+              />
+            ))}
+          </ul>
         )}
-       
-
-      
-
       </article>
     </div>
   );
