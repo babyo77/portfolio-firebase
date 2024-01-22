26c4716382db98ebdcfebd3efe19febd8bd1eb2c
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-22 06:55:20 +0530

    Update index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,7 +2,7 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { ThemeProvider } from "./theme-provider";
 import { ModeToggle } from "./mode-toggle";
-import { Container } from "./container";
+import { Container } from "./Container";
 import { Projects } from "./Projects";
 import { CustomBadge } from "./CustomBadge";
 import { Loader } from "./Loader";
