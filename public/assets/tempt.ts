34962d768c9e6f6c697f6ba23eda644aaa7289fd
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-01-23 02:39:52 +0530

    Update tempt.ts

--- public/assets/tempt.ts
+++ public/assets/tempt.ts
@@ -7,18 +7,18 @@ const friendsListtemp = [
   },
 
   {
-    "fname": "Sahil taneja 🦍( tox)",
-    "dp": "https://scontent.cdninstagram.com/v/t51.2885-19/391051637_907058970544771_7422390820788997130_n.jpg?stp=dst-jpg_s100x100&_nc_cat=102&ccb=1-7&_nc_sid=294b3b&_nc_ohc=iP4utXFRuF4AX9FNA0Z&_nc_ht=scontent.cdninstagram.com&oh=00_AfCRGw795DV5CknuKSvYxUwbpvat0pIOp_xiFl_zVE4huw&oe=65B3DC87",
-    "username": "doctor_powerlifts",
-    "url": "https://www.instagram.com/doctor_powerlifts"
-  },
-
-  {
     "fname": "sanskar singh",
     "dp": "https://scontent.cdninstagram.com/v/t51.2885-19/412371410_1025719348538858_3465420441504234428_n.jpg?stp=dst-jpg_s100x100&_nc_cat=106&ccb=1-7&_nc_sid=294b3b&_nc_ohc=hebcLi7n0B4AX_IDQ9m&_nc_ht=scontent.cdninstagram.com&oh=00_AfDhZ8JQXY8QKM0pFHGZGOcJMIMVOBcCdRHOCSQdGtMdEQ&oe=65B4812F",
     "username": "snskr_singh",
     "url": "https://www.instagram.com/snskr_singh"
   }
 ]
+
+  // {
+  //   "fname": "Sahil taneja 🦍( tox)",
+  //   "dp": "https://scontent.cdninstagram.com/v/t51.2885-19/391051637_907058970544771_7422390820788997130_n.jpg?stp=dst-jpg_s100x100&_nc_cat=102&ccb=1-7&_nc_sid=294b3b&_nc_ohc=iP4utXFRuF4AX9FNA0Z&_nc_ht=scontent.cdninstagram.com&oh=00_AfCRGw795DV5CknuKSvYxUwbpvat0pIOp_xiFl_zVE4huw&oe=65B3DC87",
+  //   "username": "doctor_powerlifts",
+  //   "url": "https://www.instagram.com/doctor_powerlifts"
+  // },
 
 export {friendsListtemp}
