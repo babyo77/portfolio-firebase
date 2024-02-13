0bcd07af8c124b391c970bcafe90d8de5d68501e
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-02-13 10:52:44 +0530

    Update Stories.tsx

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -61,7 +61,7 @@ export const Stories: React.FC<VideoLink> = ({ link, username, fullName }) => {
           <span className=" font-semibold text-lg  transition-all duration-300 hover:text-blue-300 text-left">
             {fullName}
           </span>
-          <span className=" animate-pulse ">stories here ▲</span>
+          <span className="text-sm animate-pulse ">stories here ▲</span>
         </div>
       </DialogTrigger>
       <DialogContent className="max-md:h-full backdrop-blur-md border-none">
