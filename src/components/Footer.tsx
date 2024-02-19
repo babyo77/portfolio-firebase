987435846e18d2ce39176ef8a97e0fafda3cd404
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-02-19 10:39:41 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Contact } from ".";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex justify-between items-center pt-3 pb-7">
+    <footer className="flex justify-between items-center pt-3 pb-5">
       <Contact friend={false} />
       <Contact friend={true} />
     </footer>
