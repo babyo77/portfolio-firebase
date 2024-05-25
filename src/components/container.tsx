126a93ccd29210d04f991bb1b7c4135afb5d0bcf
babyo77 <onlybabyyo7@gmail.com>
2024-05-26 02:33:35 +0530

    Update container.tsx

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,9 +1,7 @@
 import { ProjectProps } from "@/interface";
 import { Footer, Loader, Projects } from ".";
 import { Badge } from "./ui/badge";
-
 import { useLanyard } from "react-use-lanyard";
-import { AspectRatio } from "./ui/aspect-ratio";
 interface ProjectProp {
   project: ProjectProps[];
   techStack: string[];
@@ -32,36 +30,30 @@ const Container: React.FC<ProjectProp> = ({ project, techStack, discord }) => {
           ))}
         </div>
         {lanyard.data && (
-          <div className=" flex border space-x-2  items-center rounded-lg p-2">
-            <div className="h-14 w-16 relative  rounded-md ">
-              <AspectRatio ratio={1 / 1}>
-                <img
-                  className="h-14 w-14 object-cover rounded-md"
-                  src={`https://cdn.discordapp.com/app-assets/${lanyard.data.data.activities[0].application_id}/${lanyard.data.data.activities[0].assets?.large_image}.png`}
-                  onError={(e) =>
-                    (e.currentTarget.src = "/assets/favicon.webp")
-                  }
-                  alt="discord"
-                />
-                <img
-                  className="h-4 w-4  border-2 border-neutral-800 rounded-full -right-0.5  -bottom-2 z-20 absolute object-cover"
-                  src={`https://cdn.discordapp.com/app-assets/${lanyard.data.data.activities[0].application_id}/${lanyard.data.data.activities[0].assets?.small_image}.png`}
-                  onError={(e) =>
-                    (e.currentTarget.src = "/assets/favicon.webp")
-                  }
-                  alt="discord"
-                />
-              </AspectRatio>
+          <div className=" flex border   items-center rounded-lg p-2">
+            <div className="h-14 w-20 relative  rounded-md ">
+              <img
+                className="h-14 w-14 object-cover rounded-md"
+                src={`https://cdn.discordapp.com/app-assets/${lanyard.data.data.activities[0].application_id}/${lanyard.data.data.activities[0].assets?.large_image}.png`}
+                onError={(e) => (e.currentTarget.src = "/assets/favicon.webp")}
+                alt="discord"
+              />
+              <img
+                className="h-4 w-4  border-2 border-neutral-800 rounded-full right-1.5  -bottom-0.5 z-20 absolute object-cover"
+                src={`https://cdn.discordapp.com/app-assets/${lanyard.data.data.activities[0].application_id}/${lanyard.data.data.activities[0].assets?.small_image}.png`}
+                onError={(e) => (e.currentTarget.src = "/assets/favicon.webp")}
+                alt="discord"
+              />
             </div>
 
-            <div className="w-full text-xs items-center leading-tight">
+            <div className="w-full  text-xs items-center leading-tight">
               <p className="font-medium">
                 {lanyard.data.data.activities[0].name}
               </p>
-              <p className="text-zinc-300">
+              <p className="dark:text-zinc-300">
                 {lanyard.data.data.activities[0].state}
               </p>
-              <p className="text-zinc-300">
+              <p className="dark:text-zinc-300">
                 {lanyard.data.data.activities[0]?.details}
               </p>
               {/* <p className="text-zinc-300 text-[.5rem]">
