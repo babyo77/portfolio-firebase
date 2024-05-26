c83ab05fc3b911c6cbcbe457ac94a97161fb2f5d
babyo77 <onlybabyyo7@gmail.com>
2024-05-26 15:23:02 +0530

    Update container.tsx

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -32,9 +32,9 @@ const Container: React.FC<ProjectProp> = ({ project, techStack, discord }) => {
 
         {lanyard.data && (
           <div className=" flex border  bg-neutral-800/5  border-zinc-900/40 text-zinc-300  items-center rounded-sm p-2 pt-2.5">
-            <div className="h-14 w-20 relative bg-neutral-800 rounded-sm ">
+            <div className="h-14 w-20 relative  rounded-sm ">
               <img
-                className="h-14 w-14 object-cover bg-neutral-800 rounded-sm"
+                className="h-14 w-14 object-cover  rounded-sm"
                 src={`https://cdn.discordapp.com/app-assets/${lanyard.data.data.activities[0]?.application_id}/${lanyard.data.data.activities[0]?.assets?.large_image}.png`}
                 onError={(e) =>
                   (e.currentTarget.src =
