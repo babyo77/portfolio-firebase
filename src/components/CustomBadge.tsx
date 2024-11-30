c1a195b135cb505c95b98f417a7e0b9b35d977ed
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-30 23:24:01 +0530

    Update CustomBadge.tsx

--- src/components/CustomBadge.tsx
+++ src/components/CustomBadge.tsx
@@ -6,7 +6,7 @@ interface TeachStackProp {
 const CustomBadge: React.FC<TeachStackProp> = ({ techStack }) => {
   return (
     <Badge
-      variant={"outline"}
+      variant={"secondary"}
       className="font-normal group-hover:border-blue-300 transition-all duration-300 rounded-sm shadow-none mr-1"
     >
       {techStack}
