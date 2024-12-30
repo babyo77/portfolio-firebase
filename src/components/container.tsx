bd39ec5c15b112c91f31c7969182b66b6f85cc7c
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-12-30 23:29:42 +0530

    Update container.tsx

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -93,7 +93,7 @@ const Container: React.FC<ProjectProp> = ({
                 {activity?.name || "Currently offline"}
               </p>
 
-              <p className="dark:text-zinc-300 max-w-md truncate">
+              <p className="dark:text-zinc-300 max-md:max-w-xs max-w-md truncate">
                 {activity?.details}
               </p>
               <p className="dark:text-muted-foreground">{activity?.state}</p>
