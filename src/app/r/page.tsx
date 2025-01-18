aab71fc0ac941b6129efdc45a5ad5bd57938bf42
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-01-18 20:10:39 +0530

    minro changes

--- src/app/r/page.tsx
+++ src/app/r/page.tsx
@@ -3,10 +3,31 @@ import { redirect } from "next/navigation";
 type Props = {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
+
 export default async function Page({ searchParams }: Props) {
   const { url } = await searchParams;
+
   if (!url || typeof url !== "string") {
     redirect("/");
   }
+
+  const youtubeRegex =
+    /^https?:\/\/(www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)$/;
+  const youtubeMatch = youtubeRegex.exec(url);
+
+  if (youtubeMatch) {
+    const videoId = youtubeMatch[2];
+    redirect(`youtube://www.youtube.com/watch?v=${videoId}`);
+  }
+
+  const instagramRegex =
+    /^https?:\/\/(www\.)?instagram\.com\/p\/([a-zA-Z0-9_-]+)\/?$/;
+  const instagramMatch = instagramRegex.exec(url);
+
+  if (instagramMatch) {
+    const postId = instagramMatch[2];
+    redirect(`instagram://www.instagram.com/p/${postId}`);
+  }
+
   redirect(url);
 }
