62a2508610f9201cc36ac705280bd17f29328f66
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-04-12 06:11:41 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,9 +87,32 @@ const interDisplay = localFont({
   ],
 });
 export const metadata: Metadata = {
-  title: "Tanmay | Profile",
-  description: "",
+  title: "Tanmay",
+  description: "Explore Tanmay's profile — projects.",
+  openGraph: {
+    title: "Tanmay | Profile",
+    description: "Explore Tanmay's profile — projects.",
+    url: "https://tanmay.xyz/", 
+    siteName: "Tanmay's Portfolio",
+    images: [
+      {
+        url: "https://tanmay.b-cdn.net/gradii-1600x900.png", 
+        width: 1200,
+        height: 630,
+        alt: "Tanmay Profile Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Tanmay | Profile",
+    description: "Explore Tanmay's profile — projects.",
+    images: ["https://tanmay.b-cdn.net/gradii-1600x900.png"],
+  },
 };
+
 
 export default function RootLayout({
   children,
